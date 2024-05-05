3757015f2c6db6f79b7dce57a6a4b76b4a7e7db1
papesackousy <piscoentreprise@gmail.com>
2024-05-06 00:04:01 +0100

    Update user registration and login views; add additional user fields to migration
    The changes include:
    
    - Updating the user registration view to include new fields for "nom", "prenom", "adresse", and "telephone".

--- resources/views/Register.blade.php
+++ resources/views/Register.blade.php
@@ -10,26 +10,46 @@
             <div class="card mb-0">
               <div class="card-body">
                 <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="../assets/images/logos/dark-logo.svg" width="180" alt="">
+                  <img src="{{asset('Auth/assets/img/logo uabd.jpg')}}" width="180"  alt="">
                 </a>
-                <p class="text-center">Your Social Campaigns</p>
+                <p class="text-center text-primary mb-9"></p>
                 <form>
-                  <div class="mb-3">
-                    <label for="exampleInputtext1" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="textHelp">
+                  <div class="mb-2">
+                    <label for="exampleInputtext1" class="form-label">Nom</label>
+                    <input type="text"  name="nom" class="form-control" id="exampleInputtext1" aria-describedby="textHelp">
                   </div>
-                  <div class="mb-3">
+                  <div class="mb-2">
+                    <label for="exampleInputEmail1" class="form-label">Prenom</label>
+                    <input type="text"  name="prenom" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  </div>
+                  <div class="mb-2">
+                    <label for="exampleInputEmail1" class="form-label">Address</label>
+                    <input type="text"  name="adresse" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  </div>
+                  <div class="mb-2">
+                    <label for="exampleInputEmail1" class="form-label">Telephone</label>
+                    <input type="text"  name="telephone" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  </div>
+                  <div class="mb-2">
                     <label for="exampleInputEmail1" class="form-label">Email Address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="email"  name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                   </div>
-                  <div class="mb-4">
+                  <div class="mb-2">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                   </div>
-                  <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign Up</a>
+                  <div class="mb-2">
+                    <select name="role" >
+                          <option value="admin">Administrateur</option>
+                          <option value="DRH">DRH</option>
+                          <option value="Vice-Recteur">Vice-Recteur</option>
+                          <option value="Candidat">Candidat</option>
+                    </select>
+                  </div>
+                  <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Inscription</a>
                   <div class="d-flex align-items-center justify-content-center">
-                    <p class="fs-4 mb-0 fw-bold">Already have an Account?</p>
-                    <a class="text-primary fw-bold ms-2" href="./authentication-login.html">Sign In</a>
+                    <p class="fs-4 mb-0 fw-bold">Vous Avez deja un compte?</p>
+                    <a class="text-primary fw-bold ms-2" href="{{route('login')}}">Se connecte</a>
                   </div>
                 </form>
               </div>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,7 +10,7 @@
             <div class="card mb-0">
               <div class="card-body">
                 <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="../assets/images/logos/dark-logo.svg" width="180" alt="">
+                    <img src="{{asset('Auth/assets/img/logo uabd.jpg')}}" width="180"  alt="">
                 </a>
                 <p class="text-center">Your Social Campaigns</p>
                 <form>
@@ -29,12 +29,12 @@
                         Remeber this Device
                       </label>
                     </div>
-                    <a class="text-primary fw-bold" href="./index.html">Forgot Password ?</a>
+                    <a class="text-primary fw-bold" href="./index.html">Mot de pass Oublier ?</a>
                   </div>
-                  <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</a>
+                  <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Connexion</a>
                   <div class="d-flex align-items-center justify-content-center">
-                    <p class="fs-4 mb-0 fw-bold">New to Modernize?</p>
-                    <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Create an account</a>
+                    <p class="fs-4 mb-0 fw-bold">Cree un Compte?</p>
+                    <a class="text-primary fw-bold ms-2" href="{{route('register')}}">S'Inscrire</a>
                   </div>
                 </form>
               </div>
